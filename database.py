73d3862db43c11361f29997e10aebb1f795893e5
Tirupathi Rao Guggilapu <107978273+tirupathiraog@users.noreply.github.com>
2026-07-16 23:13:36 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 import os
 
-DATABASE_URL = os.getenv("postgresql://postgres:1824@localhost:5432/energy_db")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
